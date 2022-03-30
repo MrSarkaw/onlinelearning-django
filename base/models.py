06b961f975e2ce2b3e9fe15923840fa50e6686f4
sarkaw salar <sarkaw.salar55@gmail.com>
2022-03-30 20:45:15 +0300

    altering users table

--- base/models.py
+++ base/models.py
@@ -4,7 +4,13 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+    email = models.EmailField(unique=True)
+
+    avatar = models.ImageField(default = 'avatar.jpg')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 class Topic(models.Model):
     name = models.CharField(max_length=100)
